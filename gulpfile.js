dd5e074f68e88721183a88cbf85ad0f02312716e
Jeroen Baartmans <jeroen@weareorange.eu>
2024-08-02 14:11:15 +0200

    Upgrade to Bootstrap5

--- gulpfile.js
+++ gulpfile.js
@@ -5,53 +5,53 @@ const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
 
 function compileSCSS() {
-	return gulp.src('./scss/**/*.scss')				
-		.pipe(sass().on('error', sass.logError))		
+	return gulp.src('./scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./assets/css/'))
-		.pipe(minify())				
+		.pipe(minify())
 		.pipe(rename({ extname: '.min.css' }))
-		.pipe(gulp.dest('./assets/css/'));		
+		.pipe(gulp.dest('./assets/css/'));
 }
 
 function generateSourceMapCSS() {
-	return gulp.src(['./assets/css/styles.css','./assets/css/styles.min.css'])				
-    .pipe(sourcemaps.init())    
+	return gulp.src(['./assets/css/styles.css','./assets/css/styles.min.css'])
+    .pipe(sourcemaps.init())
 	.pipe(sourcemaps.write('./'))
 	.pipe(gulp.dest('assets/css/'));
 }
 
 function copyJSFiles() {
-	const jsFiles = [		
+	const jsFiles = [
 		'./node_modules/bootstrap/dist/js/bootstrap.min.js'
-		, './node_modules/bootstrap/dist/js/bootstrap.min.js.map'		
+		, './node_modules/bootstrap/dist/js/bootstrap.min.js.map'
 	]
 	return gulp.src(jsFiles)
-	.pipe(rename(function (path) {		
-		path.basename = "theme";		
+	.pipe(rename(function (path) {
+		path.basename = "theme";
 	}))
     .pipe(gulp.dest('./assets/js/'));
-}  
+}
 
-function importFontAwesomeFiles() {	
-	return gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*.*')						
-		.pipe(gulp.dest('./assets/fonts/'));		
+function importFontAwesomeFiles() {
+	return gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*.*')
+		.pipe(gulp.dest('./assets/fonts/'));
 }
 
 function copyFontFiles() {
-	return gulp.src('./fonts/**/*.*')						
-		.pipe(gulp.dest('./assets/fonts/'));		
+	return gulp.src('./assets/fonts/**/*.*')
+		.pipe(gulp.dest('./assets/fonts/'));
 }
 
 exports.buildCSS = gulp.series(compileSCSS,generateSourceMapCSS)
 exports.buildJS = gulp.series(copyJSFiles)
-exports.buildFont = gulp.series(importFontAwesomeFiles,copyFontFiles) 
-exports.copyFont = gulp.series(copyFontFiles) 
+exports.buildFont = gulp.series(importFontAwesomeFiles,copyFontFiles)
+exports.copyFont = gulp.series(copyFontFiles)
 
 exports.default = gulp.parallel(exports.buildCSS,exports.buildJS,exports.buildFont);
 exports.build = exports.default;
 
 exports.watch = function () {
-	gulp.watch('./scss/**/*.scss', exports.buildCSS);	
-	gulp.watch('./js/**/*.js', exports.buildJS);	
-	gulp.watch('./fonts/**/*.*', exports.copyFont);	
+	gulp.watch('./scss/**/*.scss', exports.buildCSS);
+	gulp.watch('./js/**/*.js', exports.buildJS);
+	gulp.watch('./fonts/**/*.*', exports.copyFont);
 };
